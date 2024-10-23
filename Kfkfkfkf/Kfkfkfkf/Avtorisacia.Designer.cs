0a5c48073134e5fa2e07ebc4f3feb93bf57e61b6
Pife <148446261+SL6428@users.noreply.github.com>
2024-10-23 16:41:59 +0300

    Add files via upload

---
+++ Kfkfkfkf/Kfkfkfkf/Avtorisacia.Designer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Kfkfkfkf
+{
+    partial class Avtorisacia
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.avto = new System.Windows.Forms.Button();
+            this.login = new System.Windows.Forms.TextBox();
+            this.pass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // avto
+            // 
+            this.avto.Location = new System.Drawing.Point(289, 266);
+            this.avto.Name = "avto";
+            this.avto.Size = new System.Drawing.Size(313, 84);
+            this.avto.TabIndex = 0;
+            this.avto.Text = "НАЖМИ";
+            this.avto.UseVisualStyleBackColor = true;
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(310, 42);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(364, 20);
+            this.login.TabIndex = 1;
+            // 
+            // pass
+            // 
+            this.pass.Location = new System.Drawing.Point(313, 126);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(376, 20);
+            this.pass.TabIndex = 2;
+            // 
+            // Avtorisacia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.avto);
+            this.Name = "Avtorisacia";
+            this.Text = "Avtorisacia";
+            this.Load += new System.EventHandler(this.Avtorisacia_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Avtorisacia_Load(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button avto;
+        private System.Windows.Forms.TextBox login;
+        private System.Windows.Forms.TextBox pass;
+    }
+}
+
---
+++ Kfkfkfkf/Kfkfkfkf/Avtorisacia.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kfkfkfkf
+{
+    public partial class Avtorisacia : Form
+    {
+        public Avtorisacia()
+        {
+            InitializeComponent();
+        }
+        static SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\as2\source\repos\Kfkfkfkf\Kfkfkfkf\Lala.mdf;Integrated Security=True");
+        private void avto_Click(object sender, EventArgs e)
+        {
+
+            int role1;
+            con.Open();
+            string query = $"SELECT * FROM lala WHERE login = N'{login.Text}'";
+            SqlCommand command = new SqlCommand(query, con);
+            SqlDataReader reader = command.ExecuteReader();
+            if ((bool)reader.Read())
+            {
+                if (reader.GetValue(2).ToString() == pass.Text)
+                {
+                    role1 = int.Parse(reader.GetValue(3).ToString());
+                    con.Close();
+                    if (role1 == 1)
+                    {
+                        student newform = new student();
+                        newform.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        prepod newform1 = new prepod();
+                        newform1.Show();
+                        this.Hide();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Несовпал парол");
+                    con.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Такого логина нема");
+                con.Close();
+            }
+            con.Close();
+        }
+
+    }
+}
+
